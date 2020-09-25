e36d2199b2f9a924105505599cea25a539995c9a
vanyakozub <59049483+vanyakozub@users.noreply.github.com>
2020-09-26 02:58:38 +0300

    Change css

--- src/main/java/servlet/SignInServlet.java
+++ src/main/java/servlet/SignInServlet.java
@@ -10,9 +10,6 @@ import java.sql.SQLException;
 import java.security.MessageDigest;
 
 
-
-
-
 public class SignInServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doPost(final javax.servlet.http.HttpServletRequest request,
@@ -52,31 +49,21 @@ public class SignInServlet extends javax.servlet.http.HttpServlet {
         pw.write("<!DOCTYPE html>");
         pw.write("<html>");
         pw.write("  <head>\n"
-                +
-                "    <meta charset=\"utf-8\">\n"
-                +
-                "    <title>Sign in</title>\n"
-                +
-                "<link rel=\"stylesheet\" href=\"First.css\">"
-                +
-                "  </head>");
+                + "    <meta charset=\"utf-8\">\n"
+                + "    <title>Sign in</title>\n"
+                + "<link rel=\"stylesheet\" href=\"FirstCSS.css\">"
+                + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css\" />"
+                + "  </head>");
         pw.write("<body>\n"
                 + "\n"
                 + "<table cellspacing=\"10\">\n"
                 + "    <tr>\n"
-                + "        <td><a href=\"\\Signin\"> Регистрация</a></td>\n"
-                + "        <td><a href=\"\\Login\"> Вход</a></td>\n"
+                + "        <td><a href=\"\\Main\"> Back</a></td>\n"
+                + "        <td><a href=\"\\Login\"> Login</a></td>\n"
                 + "    </tr>\n"
                 + "</table>");
         pw.write("<div id=\"sign\", align=\"center\">");
-        pw.write("<form method=\"POST\", action=\"Signin\">\n"
-                + "    <p>First Name <input type=\"text\", name=\"FirstName\", value=\"" + firstName +"\"></p>\n"
-                + "    <p>Last Name <input type=\"text\", name=\"LastName\", value=\"" + lastName +"\"></p>\n"
-                + "    <p>e-mail <input type=\"text\", name=\"e-mail\", value=\"" + email +"\"></p>\n"
-                + "    <p>password <input type=\"password\", name=\"password\"></p>\n"
-                + "    <p>password again <input type=\"password\", name=\"password again\"></p>\n"
-                + "    <p><input type=\"submit\" value=\"Submit!\"></p>\n"
-                + "</form>");
+        pw.write(this.getInputForm());
         if (!error.toString().isEmpty()) {
             pw.write("    <div id=\"error\">Error: " + error + "</div>");
         } else {
@@ -102,31 +89,56 @@ public class SignInServlet extends javax.servlet.http.HttpServlet {
         pw.write("<!DOCTYPE html>");
         pw.write("<html>");
         pw.write("  <head>\n"
-                +
-                "    <meta charset=\"utf-8\">\n"
-                +
-                "    <title>Sign in</title>\n"
-                +
-                "<link rel=\"stylesheet\" href=\"First.css\">"
-                +
-                "  </head>");
+                + "    <meta charset=\"utf-8\">\n"
+                + "    <title>Sign in</title>\n"
+                + "<link rel=\"stylesheet\" href=\"FirstCSS.css\">"
+                + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css\" />"
+                + "  </head>");
         pw.write("<body>\n"
                 + "\n"
                 + "<table cellspacing=\"10\">\n"
                 + "    <tr>\n"
-                + "        <td><a href=\"\\Signin\"> Регистрация</a></td>\n"
-                + "        <td><a href=\"\\Login\"> Вход</a></td>\n"
+                + "        <td><a href=\"\\Main\"> Back</a></td>\n"
+                + "        <td><a href=\"\\Login\"> Login</a></td>\n"
                 + "    </tr>\n"
                 + "</table>");
         pw.write("<div id=\"sign\", align=\"center\">");
-        pw.write("<form method=\"POST\", action=\"Signin\">\n"
-                + "    <p>First Name <input type=\"text\", name=\"FirstName\"></p>\n"
-                + "    <p>Last Name <input type=\"text\", name=\"LastName\"></p>\n"
-                + "    <p>e-mail <input type=\"text\", name=\"e-mail\"></p>\n"
-                + "    <p>password <input type=\"password\", name=\"password\"></p>\n"
-                + "    <p>password again <input type=\"password\", name=\"password again\"></p>\n"
-                + "    <p><input type=\"submit\" value=\"Submit!\"></p>\n"
-                + "</form>");
+        pw.write(this.getInputForm());
         pw.write("</div>\n</body>\n</html>");
+    }
+    public String getInputForm() {
+        return new String("<div class=\"p-x-1 p-y-3\">\n"
+                + " <form class=\"card card-block m-x-auto bg-faded form-width\" method=\"POST\", action=\"Signin\">\n"
+                + " <legend class=\"m-b-1 text-xs-center\">Registration</legend>\n"
+                + " <div class=\"form-group input-group\">\n"
+                + " <span class=\"has-float-label\">\n"
+                + " <input class=\"form-control\" id=\"first\" type=\"text\" placeholder=\"First name\" name='FirstName'/>\n"
+                + " <label for=\"first\">First name</label>\n"
+                + " </span>\n"
+                + " <span class=\"has-float-label\">\n"
+                + " <input class=\"form-control\" id=\"last\" type=\"text\" placeholder=\"Last name\" name='LastName'/>\n"
+                + " <label for=\"last\">Last name</label>\n"
+                + " </span>\n"
+                + " </div>\n"
+                + " <div class=\"form-group input-group\">\n"
+                + " <span class=\"input-group-addon\">@</span>\n"
+                + " <span class=\"has-float-label\">\n"
+                + " <input class=\"form-control\" id=\"email\" type=\"email\" placeholder=\"name@example.com\" name='e-mail'/>\n"
+                + " <label for=\"email\">E-mail</label>\n"
+                + " </span>\n"
+                + " </div>\n"
+                + " <div class=\"form-group has-float-label\">\n"
+                + " <input class=\"form-control\" id=\"password\" type=\"password\" placeholder=\"********\" name='password'/>\n"
+                + " <label for=\"password\">Password</label>\n"
+                + " </div>\n"
+                + " <div class=\"form-group has-float-label\">\n"
+                + " <input class=\"form-control\" id=\"password\" type=\"password\" placeholder=\"********\" name='password again'/>\n"
+                + " <label for=\"password\">Password again</label>\n"
+                + " </div>\n"
+                + " <div class=\"text-xs-center\">\n"
+                + " <button class=\"btn btn-block btn-primary\" type=\"submit\">Registration</button>\n"
+                + " </div>\n"
+                + " </form>\n"
+                + "</div>");
     }
 }
